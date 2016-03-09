f7e43e4c72601aeed969099ba39b9fdc8b84e6b3
vsukhin <vsukhin@virtuososoft.com>
2016-03-09 12:27:18 +0300

    Adding one more mysql db

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ running on a
       'emp_name VARCHAR(20) NOT NULL, '.
       'emp_address  VARCHAR(20) NOT NULL, '.
       'emp_salary   INT NOT NULL, '.
-      'join_date    timestamp(14) NOT NULL, '.
+      'join_date    timestamp(6) NOT NULL, '.
       'primary key ( emp_id ))';
    mysql_select_db('test_db');
    $retval = mysql_query( $sql, $conn );
@@ -50,6 +50,45 @@ running on a
    echo "Table employee created successfully\n";
    
    mysql_close($conn);
+
+   $dbhost = 'myres:3306';
+   $dbuser = 'rump';
+   $dbpass = '';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   echo 'Connected successfully to mysql server';
+   
+   $sql = 'CREATE Database example_db';
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not create database: ' . mysql_error());
+   }
+   
+   echo "Database example_db created successfully\n";
+
+   $sql = 'CREATE TABLE company( '.
+      'cmp_id INT NOT NULL AUTO_INCREMENT, '.
+      'cmp_name VARCHAR(20) NOT NULL, '.
+      'cmp_address  VARCHAR(20) NOT NULL, '.
+      'cmp_number_of_employees   INT NOT NULL, '.
+      'registration_date    timestamp(6) NOT NULL, '.
+      'primary key ( cmp_id ))';
+   mysql_select_db('example_db');
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not create table: ' . mysql_error());
+   }
+   
+   echo "Table company created successfully\n";
+   
+   mysql_close($conn);
+
 ?>
 </body>
 </html>
